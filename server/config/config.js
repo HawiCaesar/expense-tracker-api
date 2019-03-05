6e954e39bcd1c294d25e35b09787a7922523a58e
HawiCaesar <brian.odhiambo@andela.com>
2019-03-05 18:10:00 +0300

    chore(tests): set operatorsAliases to false

--- server/config/config.js
+++ server/config/config.js
@@ -1,41 +1,3 @@
-const Op = require("sequelize").Op;
-const operatorsAliases = {
-  $eq: Op.eq,
-  $ne: Op.ne,
-  $gte: Op.gte,
-  $gt: Op.gt,
-  $lte: Op.lte,
-  $lt: Op.lt,
-  $not: Op.not,
-  $in: Op.in,
-  $notIn: Op.notIn,
-  $is: Op.is,
-  $like: Op.like,
-  $notLike: Op.notLike,
-  $iLike: Op.iLike,
-  $notILike: Op.notILike,
-  $regexp: Op.regexp,
-  $notRegexp: Op.notRegexp,
-  $iRegexp: Op.iRegexp,
-  $notIRegexp: Op.notIRegexp,
-  $between: Op.between,
-  $notBetween: Op.notBetween,
-  $overlap: Op.overlap,
-  $contains: Op.contains,
-  $contained: Op.contained,
-  $adjacent: Op.adjacent,
-  $strictLeft: Op.strictLeft,
-  $strictRight: Op.strictRight,
-  $noExtendRight: Op.noExtendRight,
-  $noExtendLeft: Op.noExtendLeft,
-  $and: Op.and,
-  $or: Op.or,
-  $any: Op.any,
-  $all: Op.all,
-  $values: Op.values,
-  $col: Op.col
-};
-
 const config = {
   development: {
     username: "brianhawi",
@@ -44,7 +6,7 @@ const config = {
     host: "127.0.0.1",
     port: 5432,
     dialect: "postgres",
-    operatorsAliases: operatorsAliases
+    operatorsAliases: false
   },
   test: {
     username: "brianhawi",
@@ -53,7 +15,7 @@ const config = {
     host: "127.0.0.1",
     port: 5432,
     dialect: "postgres",
-    operatorsAliases: operatorsAliases
+    operatorsAliases: false
   },
   production: {
     username: "brianhawi",
