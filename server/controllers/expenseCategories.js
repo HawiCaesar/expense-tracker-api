168883afd9268c8d9b7322380e79d4ccaf1f12d4
HawiCaesar <brian.odhiambo@andela.com>
2019-03-13 20:51:48 +0300

    chore(lines): remove unnecessary lines

--- server/controllers/expenseCategories.js
+++ server/controllers/expenseCategories.js
@@ -39,41 +39,39 @@ module.exports = {
           as: "expenses"
         }
       ]
-    })
-      .then(category => {
-        if (!category) {
-          return response.status(404).send({
-            message: "Expense category not found"
-          });
-        }
-        return Expense.findOne({
-          where: {
-            expenseCategoryId: request.params.expenseCategoryId
-          },
-          attributes: [
-            "expenseCategoryId",
-            [sequelize.fn("sum", sequelize.col("amount")), "total"]
-          ],
-          group: ["expense.expenseCategoryId"]
-        })
-          .then(summedExpenses => {
-            let totalExpenses = 0;
-            if (summedExpenses !== null) {
-              totalExpenses = +summedExpenses.dataValues.total;
-            }
-            return response.status(200).send({
-              id: category.id,
-              name: category.name,
-              description: category.description,
-              updatedAt: category.updatedAt,
-              createdAt: category.createdAt,
-              expensesTotal: totalExpenses,
-              expenses: category.expenses
-            });
-          })
-          .catch(error => response.status(400).send({ message: `${error}` }));
+    }).then(category => {
+      if (!category) {
+        return response.status(404).send({
+          message: "Expense category not found"
+        });
+      }
+      return Expense.findOne({
+        where: {
+          expenseCategoryId: request.params.expenseCategoryId
+        },
+        attributes: [
+          "expenseCategoryId",
+          [sequelize.fn("sum", sequelize.col("amount")), "total"]
+        ],
+        group: ["expense.expenseCategoryId"]
       })
-      .catch(error => response.status(400).send({ message: `${error}` }));
+        .then(summedExpenses => {
+          let totalExpenses = 0;
+          if (summedExpenses !== null) {
+            totalExpenses = +summedExpenses.dataValues.total;
+          }
+          return response.status(200).send({
+            id: category.id,
+            name: category.name,
+            description: category.description,
+            updatedAt: category.updatedAt,
+            createdAt: category.createdAt,
+            expensesTotal: totalExpenses,
+            expenses: category.expenses
+          });
+        })
+        .catch(error => response.status(400).send({ message: `${error}` }));
+    });
   },
   update(request, response) {
     return ExpenseCategory.findOne({
@@ -120,41 +118,5 @@ module.exports = {
         .then(() => response.status(204).send())
         .catch(error => response.status(400).send({ message: `${error}` }));
     });
-  },
-  retrieveSummary(request, response) {
-    return ExpenseCategory.findOne({
-      where: {
-        id: request.params.expenseCategoryId,
-        ownerId: request.user.id
-      }
-    })
-      .then(category => {
-        return Expense.findOne({
-          where: {
-            expenseCategoryId: request.params.expenseCategoryId
-          },
-          attributes: [
-            "expenseCategoryId",
-            [sequelize.fn("sum", sequelize.col("amount")), "total"]
-          ],
-          group: ["expense.expenseCategoryId"]
-        })
-          .then(expense => {
-            return response.status(200).send({
-              id: category.id,
-              name: category.name,
-              description: category.description,
-              updatedAt: category.updatedAt,
-              createdAt: category.createdAt,
-              expensesTotal: expense.dataValues.total
-            });
-          })
-          .catch(error => {
-            response.status(400).send({ message: `${error}` });
-          });
-      })
-      .catch(error => {
-        response.status(404).send({ message: `${error}` });
-      });
   }
 };
