3361b1bae40e0ad19bbe00e0b26ae85fe446cee5
HawiCaesar <brian.odhiambo@andela.com>
2019-03-14 14:11:52 +0300

    feat(heroku): add config setting for heroku

--- server/config/config.js
+++ server/config/config.js
@@ -8,7 +8,8 @@ const config = {
     host: "127.0.0.1",
     port: 5432,
     dialect: "postgres",
-    operatorsAliases: false
+    operatorsAliases: false,
+    use_env_variable: process.env.DATABASE_URL
   },
   test: {
     username: process.env.TEST_DATABASE_USERNAME,
